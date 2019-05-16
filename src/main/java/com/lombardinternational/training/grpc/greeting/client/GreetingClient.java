e60b75003e229b57f2cc35500e3872c6f3ca2037
Mohamed BELARBI <belarbim@gmail.com>
2019-05-16 12:29:47 +0200

    Create server and client

---
+++ src/main/java/com/lombardinternational/training/grpc/greeting/client/GreetingClient.java
@@ -0,0 +1,4 @@
+package com.lombardinternational.training.grpc.greeting.client;
+
+public class GreetingClient {
+}
---
+++ src/main/java/com/lombardinternational/training/grpc/greeting/server/GreetingServer.java
@@ -0,0 +1,4 @@
+package com.lombardinternational.training.grpc.greeting.server;
+
+public class GreetingServer {
+}
