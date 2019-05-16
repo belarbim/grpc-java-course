c01a92fcbd60f16709806459872b96141b848075
Mohamed BELARBI <belarbim@gmail.com>
2019-05-16 13:22:10 +0200

    bi-directionnel streaming

--- src/main/java/com/lombardinternational/training/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/lombardinternational/training/grpc/greeting/client/GreetingClient.java
@@ -6,6 +6,7 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
+import java.util.Arrays;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -22,9 +23,10 @@ public class GreetingClient {
         final ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
-        callGreetServiceUnary(channel);
-        doServerStreamingCall(channel);
-        doClientStreamingCall(channel);
+       // callGreetServiceUnary(channel);
+        //doServerStreamingCall(channel);
+        //doClientStreamingCall(channel);
+        doBiDiStreamingCall(channel);
         System.out.println("ShutingDown channel");
         channel.shutdown();
 
@@ -120,4 +122,53 @@ public class GreetingClient {
             e.printStackTrace();
         }
     }
+
+    private void doBiDiStreamingCall(ManagedChannel channel) {
+        GreetServiceGrpc.GreetServiceStub asyncClient = GreetServiceGrpc.newStub(channel);
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        StreamObserver<GreetEveryoneRequest> requestObserver = asyncClient.greetEveryone(new StreamObserver<GreetEveryoneResponse>() {
+            @Override
+            public void onNext(GreetEveryoneResponse value) {
+                System.out.println("Response from server: " + value.getResult());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                latch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Server is done sending data");
+                latch.countDown();
+            }
+        });
+
+        Arrays.asList("Stephane", "John", "Marc", "Patricia").forEach(
+                name -> {
+                    System.out.println("Sending: " + name);
+                    requestObserver.onNext(GreetEveryoneRequest.newBuilder()
+                            .setGreeting(Greeting.newBuilder()
+                                    .setFirstName(name))
+                            .build());
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+
+        requestObserver.onCompleted();
+
+        try {
+            latch.await(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
